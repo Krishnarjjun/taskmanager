82845f418d4ce148aa574279d0fbd02d072d122c
Krishna <kondrakrishna97@gmail.com>
2025-02-11 22:27:48 +0530

    Initial commit: Django project setup

---
+++ tasks/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,redirect, get_object_or_404
+from .models import Task
+
+# Display all tasks
+def task_list(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+# Create a new task
+def task_create(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        Task.objects.create(title=title, description=description)
+        return redirect('task_list')
+    return render(request, 'tasks/task_form.html')
+
+def task_update(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.title = request.POST.get('title')
+        task.description = request.POST.get('description')
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/task_form.html', {'task': task})
+# Delete task view
+def task_delete(request, task_id):
+    task = get_object_or_404(Task, id=task_id)
+    if request.method == 'POST':
+        task.delete()
+        return redirect('task_list')
+    return render(request, 'tasks/task_confirm_delete.html', {'task': task})
